04e870adf3c65e86230e8c0ed3d34b4a47b140ee
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-09 17:42:21 +0100

    Command text

--- commands/commands.js
+++ commands/commands.js
@@ -11,7 +11,7 @@ run: async (bot, message, args) => {
     
     let commandsEmbed = new Discord.RichEmbed()
         .setTitle("**Commands**")
-        .setDescription("**!help** \n **!commands** \n **!botinfo** \n **!serverinfo** \n **!kick** \n **!ban** \n **!warn** \n **!warnings** \n **!tempmute** \n **!say** \n **!report** \n **!removerole** \n **!addrole** \n **!8ball** ");
+        .setDescription("```**!help** \n **!commands** \n **!botinfo** \n **!serverinfo** \n **!kick** \n **!ban** \n **!warn** \n **!warnings** \n **!tempmute** \n **!say** \n **!report** \n **!removerole** \n **!addrole** \n **!8ball**```");
 
         
         
