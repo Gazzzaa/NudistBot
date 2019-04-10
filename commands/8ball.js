da4a51be7095a5480b8269bf0799a5aeb8e6259c
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-10 09:47:36 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -10,7 +10,7 @@ module.exports = {
 run: async (bot, message, args) => {
 
 if(!args[0]) return message.reply("Enter a question for the magic 8ball!");
-let replies = ["Yes.","No.","I don't know.","Maybe.","Ask me later."];
+let replies = ["Yes","No","I don't know","Maybe","Ask me again later","It is certain","Without a doubt","As I see it, yes","Most likely","Cannot predict now","Concentrate and ask again","Don’t count on it","Very doubtful","My sources say no"];
 if(args[0] == "help"){
 
     let Embed = new Discord.RichEmbed()
@@ -23,7 +23,13 @@ if(args[0] == "help"){
     message.channel.send(Embed);
     return;
 }
- 
+ if(args[0] == "replies"){
+    let repliesEmbed = new Discord.RichEmbed()
+    .setTitle("_8Ball Replies List_")
+    .setDescription("Yes \n No \n I don't know \n Maybe \n Ask me again later \n It is certain \n Without a doubt \n As I see it, yes \n Most likelyCannot predict now \n Concentrate and ask again \n Don’t count on it \n Very doubtful \n My sources say no");
+    message.channel.send(repliesEmbed);
+    return;
+}
 
 let result = Math.floor((Math.random() * replies.length));
 let question = args.join(" ");
