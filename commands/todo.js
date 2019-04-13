07a0e8c08a6a22d24244e3ae11cfaaf16c482da9
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-13 18:28:04 +0100

    maybe

---
+++ commands/todo.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    help: {
+        name: "todo",
+        description: "Shows information about the bot",
+        usage: "!botinfo",
+        command: "!botinfo",
+        aliases: ["binfo"],
+        aliasname: "!binfo",
+
+    },
+
+run: async (bot, message, args) => {
+
+    if(args[0] == "help"){
+
+        let Embed = new Discord.RichEmbed()
+        .setTitle("BotInfo")
+        .setColor("#FF0000")
+        .addField("Command: ","!botinfo" )
+        .addField(`Usage: `,module.exports.help.usage)
+        .addField("Description: ", module.exports.help.description)
+        .addField("Aliases: ",module.exports.help.aliasname)
+        .setTimestamp();
+        message.channel.send(Embed);
+        return;
+    }
+        message.channel.send(message.id(565503319641030657));
+       
+      
+    
+    
+}
+}
