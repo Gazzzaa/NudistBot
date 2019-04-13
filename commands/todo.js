5cae7b38419606bd89afb380148e6a87898af159
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-13 18:42:45 +0100

    Update todo.js

--- commands/todo.js
+++ commands/todo.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 
 module.exports = {
     help: {
-        name: "todo",
-        description: "Shows information about the bot",
-        usage: "!botinfo",
-        command: "!botinfo",
-        aliases: ["binfo"],
-        aliasname: "!binfo",
+        name: "todobot",
+        description: "Bots todo list",
+        usage: "!todobot",
+        command: "!todobot",
+        aliases: [],
+        aliasname: "",
 
     },
 
@@ -17,18 +17,18 @@ run: async (bot, message, args) => {
     if(args[0] == "help"){
 
         let Embed = new Discord.RichEmbed()
-        .setTitle("BotInfo")
+        .setTitle("Todod List Bot")
         .setColor("#FF0000")
-        .addField("Command: ","!botinfo" )
+        .addField("Command: ","!todobot" )
         .addField(`Usage: `,module.exports.help.usage)
         .addField("Description: ", module.exports.help.description)
-        .addField("Aliases: ",module.exports.help.aliasname)
         .setTimestamp();
         message.channel.send(Embed);
         return;
     }
         message.channel.fetchMessage(`565503319641030657`)
         .then(message => message.channel.send(message.content));
+        message.channel.send(`https://discordapp.com/channels/410961369945341962/563096148538163228/565503319641030657`);
        
       
     
