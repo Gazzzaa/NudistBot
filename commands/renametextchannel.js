7fe6d256fe56528f4fd85acc4d39195287e9ad41
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-13 15:44:44 +0100

    rename channel

---
+++ commands/renametextchannel.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    help: {
+        name: "renametxtchannel",
+        description: "Renames a text channel",
+        usage: "!renametxtchannel <name>",
+        command: "!renametxtchannel",
+        aliases: ["rtc"],
+        aliasname: "!rtc",
+
+    },
+
+run: async (bot, message, args) => {
+
+    if(args[0] == "help"){
+
+        let Embed = new Discord.RichEmbed()
+        .setTitle("Rename text channel")
+        .setColor("#FF0000")
+        .addField("Command: ","!renametxtchannel" )
+        .addField(`Usage: `,module.exports.help.usage)
+        .addField("Description: ", module.exports.help.description)
+        .addField("Aliases: ",module.exports.help.aliasname)
+        .setTimestamp();
+        message.channel.send(Embed);
+        return;
+    }else message.channel.send("Enter a channel name") & args[0] == channelname;
+    
+        let channelname;
+        channel.setName(channelname);
+        message.channel.send("This channel name has been chnaged to ", channelname);
+
+
+      
+    
+    
+}
+}
