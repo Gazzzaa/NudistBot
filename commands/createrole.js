8a9dcdd1704d73faa35ed4a13449a88456aeaf45
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-14 00:56:18 +0100

    Update createrole.js

--- commands/createrole.js
+++ commands/createrole.js
@@ -34,7 +34,8 @@ run: async (bot, message, args) => {
         }
         else if (!args[1]){
 
-            message.reply("Enter a valid color! \n DEFAULT, AQUA, GREEN, BLUE, PURPLE, LUMINOUS_VIVID_PINK, GOLD, ORANGE, RED, GREY, DARKER_GREY, NAVY, DARK_AQUA, DARK_GREEN, DARK_BLUE, DARK_PURPLE, DARK_VIVID_PINK, DARK_GOLD, DARK_ORANGE, DARK_RED, DARK_GREY, LIGHT_GREY, DARK_NAVY, RANDOM")
+            message.reply("Enter a valid color!")
+            message.channel.send("DEFAULT, AQUA, GREEN, BLUE, PURPLE, LUMINOUS_VIVID_PINK, GOLD, ORANGE, RED, GREY, DARKER_GREY, NAVY, DARK_AQUA, DARK_GREEN, DARK_BLUE, DARK_PURPLE, DARK_VIVID_PINK, DARK_GOLD, DARK_ORANGE, DARK_RED, DARK_GREY, LIGHT_GREY, DARK_NAVY, RANDOM");
         }
         
         else{
