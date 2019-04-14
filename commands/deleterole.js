0031316d79c84bec7a92c6a86c972ce77a29e69b
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-14 14:08:17 +0100

    Update deleterole.js

--- commands/deleterole.js
+++ commands/deleterole.js
@@ -40,7 +40,7 @@ run: async (bot, message, args) => {
         .addField("__Deleted By__", message.author)
         .setTimestamp();
         
-        message.role.delete(rolename);
+        message.guild.role.delete(rolename);
         message.channel.send(roleEmbed);    
         }
 
