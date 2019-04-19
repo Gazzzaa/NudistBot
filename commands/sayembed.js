07d303e48450600a48d150b24de148287d08fb90
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-19 23:01:59 +0100

    Create sayembed.js

---
+++ commands/sayembed.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    help: {
+        name: "sayembed",
+        description: "Outputs an embed",
+        usage: ".sayembed",
+        command: ".sayembed",
+        aliases: ["saye"],
+        aliasname: ".saye",
+
+    },
+
+run: async (bot, message, args) => {
+
+    if(args[0] == "help"){
+
+        let Embed = new Discord.RichEmbed()
+        .setTitle("Say embed")
+        .setColor("#FF0000")
+        .addField("Command: ",module.exports.help.command )
+        .addField(`Usage: `,module.exports.help.usage)
+        .addField("Description: ", module.exports.help.description)
+        .addField("Aliases: ",module.exports.help.aliasname)
+        .setTimestamp();
+        message.channel.send(Embed);
+        return;
+    }
+    if(!args[0]){
+        message.channel.send("Enter a title");
+    }else title = args[0]
+    if(!args[1]){
+        message.channel.send("Enter a message");
+    }else msg = args.join(" ").slice(1);
+
+        let sayEmbed = new Discord.RichEmbed()
+        .setTitle(title)
+        .setColor("#FF0000")
+        .setDescription(msg);
+        message.channel.send(sayEmbed);
+      
+    
+    
+}
+}
