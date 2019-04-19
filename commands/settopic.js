c102a5c49026dbb0e992aa457ecd888a0e7d2198
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-19 22:19:24 +0100

    settopic

---
+++ commands/settopic.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    help: {
+        name: "settopic",
+        description: "Sets the channel topic",
+        usage: "!settopic",
+        command: "!settopic",
+        aliases: [],
+        aliasname: "st",
+
+    },
+
+run: async (bot, message, args) => {
+    if(args[0] == "help"){
+
+    let Embed = new Discord.RichEmbed()
+    .setTitle("Set Topic")
+    .setColor("#FF0000")
+    .addField("Command: ","!settopic" )
+    .addField(`Usage: `,module.exports.help.usage)
+    .addField("Description: ", module.exports.help.description)
+    .addField("Aliases: ",module.exports.help.aliasname)
+    .setTimestamp();
+    message.channel.send(Embed);
+    return;
+    }else channeltopic = args[0]
+
+     let channelEmbed = new Discord.RichEmbed()
+        .setColor("#FF0000")
+        .addField("New Channel Topic ",channeltopic)
+        .addField("Changed By ", message.author)
+        .setTimestamp();
+        
+        message.channel.setTopic(channeltopic);
+        message.channel.send(channelEmbed);    
+}
+}
