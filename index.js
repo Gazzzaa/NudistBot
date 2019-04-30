4561611a714137e47f55d769cd568a8a7a5ddb3c
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-30 20:38:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ fs.readdir("./commands/reactions/", (err, files) =>{
 
 bot.on("ready", async () =>{
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("Ark Survival Evolved");
+  bot.user.setActivity(".commands");
 });
 
 bot.on("guildMemberAdd", async member =>{
