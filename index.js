5d4932759be756a173decd32af1679fe14635fde
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-04-30 20:18:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on("guildMemberAdd", async member =>{
   let verifiRole = member.guild.roles.find(r => r.name === "Verifi");
   console.log(`${member.id} joined the discord`);
   let joinChannel = member.guild.channels.find(c => c.name === "verification");
-  joinChannel.send(`Welcome to nudist discord ${member}, ${verifiRole} will verifi you soon!`);
+  joinChannel.send(`Welcome to the discord ${member}, ${verifiRole} will verifi you soon!`);
   let needsverifiRole = member.guild.roles.find(r => r.name === "Needs Verified");
   await(member.addRole(needsverifiRole.id));
 
