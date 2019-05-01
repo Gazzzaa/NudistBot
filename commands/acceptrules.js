e6a76786d50fda9bdb90e8165887ba2b99708c2d
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-05-02 00:11:08 +0100

    Update acceptrules.js

--- commands/acceptrules.js
+++ commands/acceptrules.js
@@ -11,9 +11,9 @@ module.exports = {
     },
 
 run: async (bot, message, args) => {
-let rrUser = message.mentions.members.first()
+let rrUser = message.guild.fetchMember(message.member);
 rrUser.removeRole(`573256244283048006`);
-if(!rrUser) return
+
 
     
 
