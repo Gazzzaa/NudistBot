a6cb2102e4b813db95225de7425f91495c8417d7
Gazzzaa <37639288+Gazzzaa@users.noreply.github.com>
2019-10-10 11:50:17 +0100

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -24,6 +24,7 @@ run: async (bot, message, args) => {
         return;
     }
         if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MEMBERS");
+    let num = args[0] + 1;
         if(!args[0]) return message.channel.send("Enter a specfic number");
         message.channel.bulkDelete(args[0]).then(() =>{
         message.channel.send(`Cleared ${args[0]} messages`).then(msg => msg.delete(5000));
